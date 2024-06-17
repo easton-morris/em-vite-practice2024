382a087bc636ef192a3da15fab3f93c2c2a7151b
Easton Morris <easton.morris@gmail.com>
2024-06-16 22:23:28 -0700

    added MUI app bar and tested customizations and react updates for classes

--- vite-practice-2024/src/components/AppNav.tsx
+++ vite-practice-2024/src/components/AppNav.tsx
@@ -1,18 +1,140 @@
 import React from "react";
 
-import { AppBar } from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import { AcUnit, Adb } from "@mui/icons-material";
 import iWebAppBarProps from "./WebAppBarProps";
 
 export interface WebAppBarState {
   viewMode: string;
+  pages: Array<string>;
+  anchorElNav: HTMLElement | null;
 }
 
 export default class WebAppBar extends React.Component<iWebAppBarProps, WebAppBarState> {
 
-  render(): React.ReactNode {
-    return (
-      <AppBar>
+  constructor(props: iWebAppBarProps) {
+    super(props);
 
+    this.state = {
+      viewMode: "display",
+      pages: ['Products', 'Pricing', 'Blog'],
+      anchorElNav: null
+    }
+
+    this.handleOpenNavMenu = this.handleOpenNavMenu.bind(this);
+  }
+
+  private handleOpenNavMenu (event: React.MouseEvent<HTMLElement>): void {
+    this.setState({anchorElNav: event.currentTarget});
+  }
+
+  private handleCloseNavMenu(): void {
+    this.setState({anchorElNav: null});
+  }
+
+  render(): React.ReactNode {
+
+    return (
+      <AppBar position="static">
+        <Container maxWidth='xl'>
+          <Toolbar disableGutters>
+            <AcUnit sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={this.handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={this.state.anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(this.state.anchorElNav)}
+                onClose={this.handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {this.state.pages.map((page) => (
+                  <MenuItem key={page} onClick={this.handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Adb sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {this.state.pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={this.handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+          </Toolbar>
+        </Container>
       </AppBar>
     );
   }
